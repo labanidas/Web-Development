ea2eb6b649a1d957c00096b8ff904e20206d5d57
Labani Das <labanidas2001@gmail.com>
2022-02-01 16:22:38 +0530

    Restored files

---
+++ ceil.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+/* The seiling function under cmath library finds the lowest integer grater than the given number*/
+
+int main(){
+	cout<<ceil(1.2)<<endl;
+	int n=5;
+	cout<<ceil((float)n/2)<<endl;
+}
--- sum.py
+++
@@ -1,3 +0,0 @@
-#the sum() function returns the sum of the elements in a list
-thislist=[1,2,3,4]
-print(sum(thislist))
