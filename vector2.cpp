e543ae6d3256664612cc14a539d1546aa0daf3d8
Labani Das <labanidas2001@gmail.com>
2022-02-02 20:01:30 +0530

    end() and begin() functions of vector

---
+++ vector2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> vec {1,2,3,4,5,6};
+    for(auto i=vec.begin(); i!=vec.end(); i++) cout<<*i<<' ';
+ }
+ 
+
